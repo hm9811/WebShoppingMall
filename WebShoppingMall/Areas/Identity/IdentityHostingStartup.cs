97c6909bff7e7cde8f6778fb6df8f9469879c320
Jaehyun Lewis <72472484+hm9811@users.noreply.github.com>
2021-04-14 14:01:40 -0700

    Issue#20 customize identity (#21)
    * Scaffolded all ASP .NET Identity pages
    
    * Deleted the applicationDBContext because there was two applicationDbContext file in one project
    
    * Changed required account and modified password requirements
    
    * Modified lockout options
    
    * Modified register page to allow username and email separately
    
    * Change login to require username instead of email
    
    * remove external login providers from login page

---
+++ WebShoppingMall/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebShoppingMall.Data;
+
+[assembly: HostingStartup(typeof(WebShoppingMall.Areas.Identity.IdentityHostingStartup))]
+namespace WebShoppingMall.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ApplicationDbContextConnection")));
+
+            });
+        }
+    }
+}
