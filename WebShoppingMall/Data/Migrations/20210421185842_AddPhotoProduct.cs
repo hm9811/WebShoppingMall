ad4a54418614d877cc8a3b35934902a567f1383d
hm9811 <hm981198@gmail.com>
2021-04-21 11:59:19 -0700

    Add migration for photo product

---
+++ WebShoppingMall/Data/Migrations/20210421185842_AddPhotoProduct.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebShoppingMall.Data.Migrations
+{
+    public partial class AddPhotoProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PhotoProducts",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Descriptions = table.Column<string>(nullable: true),
+                    PhotoId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhotoProducts", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhotoProducts");
+        }
+    }
+}
