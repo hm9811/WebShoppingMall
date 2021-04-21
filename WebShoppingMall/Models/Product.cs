e2eb7653c95c860fc2156c278a60c8c35a0eaeb9
hm9811 <hm981198@gmail.com>
2021-04-21 11:57:59 -0700

    Create Photo Product model

---
+++ WebShoppingMall/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShoppingMall.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Descriptions { get; set; }
+    }
+
+    public class PhotoProduct : Product
+    {
+        public string PhotoId { get; set; }
+    }
+}
