f3d6ca1ab7d8ef45cf8fb666342a0f49f835d50d
hm9811 <hm981198@gmail.com>
2021-05-06 17:59:42 -0700

    Created model and added migration

---
+++ WebShoppingMall/Models/TagProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShoppingMall.Models
+{
+    public class TagProduct
+    {
+        [Key]
+        public int TagId { get; set; }
+        public string Name { get; set; }
+    }
+}
