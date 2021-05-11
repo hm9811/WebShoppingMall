b89e19bc3c2ea2310ec6129ce7bfb306e6a6a6e2
hm9811 <hm981198@gmail.com>
2021-05-10 22:26:10 -0700

    Added Model for Product List that is connect with Product Type and Product tag.

---
+++ WebShoppingMall/Data/Migrations/20210511052028_addProductModel.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebShoppingMall.Data.Migrations
+{
+    public partial class addProductModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: false),
+                    Price = table.Column<double>(nullable: false),
+                    Image = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    IsAvailable = table.Column<bool>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    TagId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_PhotoProducts_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "PhotoProducts",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Products_TagProduct_TagId",
+                        column: x => x.TagId,
+                        principalTable: "TagProduct",
+                        principalColumn: "TagId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductId",
+                table: "Products",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_TagId",
+                table: "Products",
+                column: "TagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ WebShoppingMall/Models/ProductList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShoppingMall.Models
+{
+    public class ProductList
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        public string Image { get; set; }
+
+        [Display(Name = "Color Options")]
+        public string Color { get; set; }
+
+        [Required]
+        [Display(Name = "Available")]
+        public bool IsAvailable { get; set; }
+
+        [Required]
+        [Display(Name = "Product Type")]
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public PhotoProduct ProductTypes { get; set; }
+
+        [Required]
+        [Display(Name = "Tag")]
+        public int TagId { get; set; }
+
+        [ForeignKey("TagId")]
+        public TagProduct ProductTag { get; set; }
+    }
+}
